e9a1e89f2dd6dcb50c80268aad7cf9baafbcbd49
Hulliskoa <trulsistre@gmail.com>
2019-01-15 18:47:48 +0100

    uploaded files

---
+++ script/script.js
@@ -0,0 +1,24 @@
+function redirectStart(){
+	if(document.getElementById("inputStart").value.toLowerCase() == "start"){
+		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl1.html';
+	}
+}
+
+function redirectLvl1(){
+	if(document.getElementById("inputLvl1").value.toLowerCase() == "start"){
+		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl2.html';
+	}
+}
+
+
+function redirectLvl2(){
+	if(document.getElementById("inputLvl1").value.toLowerCase() == "start"){
+		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl3.html';
+	}
+}
+
+function redirectLvl3(){
+	if(document.getElementById("inputLvl1").value.toLowerCase() == "start"){
+		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl4.html';
+	}
+}
