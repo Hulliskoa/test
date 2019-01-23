995e02d96eb587f12a8627900e8102214a24aa0f
Hulliskoa <trulsistre@gmail.com>
2019-01-23 13:30:49 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,33 @@
+function redirectStart(){
+	if(document.getElementById("inputStart").value.toLowerCase() == "start"){
+		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl1.html';
+	}
+}
+
+function redirectLvl1(){
+	if(document.getElementById("inputLvl1").value.toLowerCase() == "challenge"){
+		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl2.html';
+	}
+}
+
+
+function redirectLvl2(){
+	var input = document.getElementById("inputLvl1").value.toLowerCase();
+	if( input == "learn" || input == "block" || input == "escape"){
+		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl3.html';
+	}
+}
+
+function redirectLvl3(){
+	var input = document.getElementById("inputLvl1").value.toLowerCase();
+	if( input == "learn" || input == "block" || input == "escape"){
+		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl4.html';
+	}
+}
+
+function redirectLvl4(){
+	if(document.getElementById("inputLvl1").value.toLowerCase() == "gameified"){
+		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/final.html';
+	}
+}
+
