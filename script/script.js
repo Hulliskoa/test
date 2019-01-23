765eb49d26dae18ed297b27c3f6420b4b7f186c8
Hulliskoa <trulsistre@gmail.com>
2019-01-23 11:00:34 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
 function redirectStart(){
 	if(document.getElementById("inputStart").value.toLowerCase() == "start"){
-		window.location.href = 'https://hulliskoa.github.io/test/lvl1.html';
+		window.location.href = 'https://hulliskoa.github.io/theGame/lvl1.html';
 	}
 }
 
 function redirectLvl1(){
 	if(document.getElementById("inputLvl1").value.toLowerCase() == "start"){
-		window.location.href = 'https://hulliskoa.github.io/test/lvl2.html';
+		window.location.href = 'https://hulliskoa.github.io/theGame/lvl2.html';
 	}
 }
 
 
 function redirectLvl2(){
 	if(document.getElementById("inputLvl1").value.toLowerCase() == "start"){
-		window.location.href = 'https://hulliskoa.github.io/test/lvl3.html';
+		window.location.href = 'https://hulliskoa.github.io/theGame/lvl3.html';
 	}
 }
 
 function redirectLvl3(){
 	if(document.getElementById("inputLvl1").value.toLowerCase() == "start"){
-		window.location.href = 'https://hulliskoa.github.io/test/lvl4.html';
+		window.location.href = 'https://hulliskoa.github.io/theGame/lvl4.html';
 	}
 }
