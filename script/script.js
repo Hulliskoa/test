ab77d3933d8ba5448943cd45f0f47a7356de7ee2
Hulliskoa <trulsistre@gmail.com>
2019-01-23 12:08:53 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 function redirectStart(){
 	if(document.getElementById("inputStart").value.toLowerCase() == "start"){
-		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl1.html';
+		window.location.href = 'https://hulliskoa.github.io/theGame/lvl1';
 	}
 }
 
 function redirectLvl1(){
 	if(document.getElementById("inputLvl1").value.toLowerCase() == "challenge"){
-		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl2.html';
+		window.location.href = 'https://hulliskoa.github.io/theGame/lvl2';
 	}
 }
 
@@ -14,18 +14,18 @@ function redirectLvl1(){
 function redirectLvl2(){
 	var input = document.getElementById("inputLvl1").value.toLowerCase();
 	if( input == "learn" || input == "block" || input == "escape"){
-		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl3.html';
+		window.location.href = 'https://hulliskoa.github.io/theGame/lvl3';
 	}
 }
 
 function redirectLvl3(){
 	if(document.getElementById("inputLvl1").value.toLowerCase() == "start"){
-		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/lvl4.html';
+		window.location.href = 'https://hulliskoa.github.io/theGame/lvl4';
 	}
 }
 
 function redirectLvl4(){
 	if(document.getElementById("inputLvl1").value.toLowerCase() == "start"){
-		window.location.href = 'file:///C:/Users/Truls/Desktop/ReadyPlayerOne/final.html';
+		window.location.href = 'https://hulliskoa.github.io/theGame/final';
 	}
 }
